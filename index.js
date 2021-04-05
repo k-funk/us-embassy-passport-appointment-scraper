b865a21676ff6dbe1cdbdc659f8ff4f2c7ad529c
Kevin Funk <kfunk@hazel.co>
2021-04-05 10:16:34 -0600

    use fetch api instead of request-promise-native

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import rp from 'request-promise-native' // TODO: this package is deprecated. use another
+import fetch from 'node-fetch'
 import chalk from 'chalk'
 import cheerio from 'cheerio'
 import notifier from 'node-notifier'
@@ -12,28 +12,28 @@ const YEAR = 2021
 const CALL_INTERVAL = 1000 * 60 * 5 // 5 minutes
 
 async function getCookieAndCSRFToken() {
+  let response
   try {
-    const resp = await rp({
-      uri: 'https://evisaforms.state.gov/acs/default.asp?postcode=SNJ&appcode=1',
-      resolveWithFullResponse: true,
-    })
-
-    const cookie = resp.headers['set-cookie'][0].split(';')[0] // get the first cookie
-    // could use cheerio, but evaluating all html as a string seems easier. the same CSRF token
-    // appears twice on the page, in this same format
-    const csrfToken = resp.body.match(CSRF_REGEX)[1] // index 1 is the first group match
-
-    if (!cookie || ! csrfToken) { throw new Error() }
-
-    return { cookie, csrfToken }
+    response = await fetch('https://evisaforms.state.gov/acs/default.asp?postcode=SNJ&appcode=1')
+    if (!response.ok) throw Error(response.status)
   } catch(err) {
-    throw new Error('Failed to get cookie or csrf token')
+    throw new Error(`Failed to get cookie or csrf token${err.code ? `: ${err.code}` : `\n${JSON.stringify(err)}`}`)
   }
+
+  const text = await response.text()
+
+  const cookie = response.headers.get('set-cookie').split(';')[0] // get the first cookie
+  // could use cheerio, but evaluating all html as a string seems easier. the same CSRF token
+  // appears twice on the page, in this same format
+  const csrfToken = text.match(CSRF_REGEX)[1] // index 1 is the first group match
+
+  if (!cookie || ! csrfToken) { throw new Error('Unable to extract cookie/csrf token from response') }
+
+  return { cookie, csrfToken }
 }
 
 function makeMonthRequestPromise(cookie, csrfToken, month) {
-  return rp({
-    uri: `https://evisaforms.state.gov/acs/make_calendar.asp?CSRFToken=${csrfToken}&nMonth=${month}&nYear=${YEAR}&type=1&servicetype=06&pc=SNJ`,
+  return fetch(`https://evisaforms.state.gov/acs/make_calendar.asp?CSRFToken=${csrfToken}&nMonth=${month}&nYear=${YEAR}&type=1&servicetype=06&pc=SNJ`, {
     headers: {
       'Connection': 'keep-alive',
       'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
@@ -49,44 +49,55 @@ function makeMonthRequestPromise(cookie, csrfToken, month) {
       'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
       'Cookie': cookie,
     },
-    json: true
   })
 }
 
+async function fetchMonths(cookie, csrfToken) {
+  const requests = MONTHS.map(month => makeMonthRequestPromise(cookie, csrfToken, month))
+  try {
+    const responses = await Promise.all(requests)
+    responses.forEach(response => { if (!response.ok) throw new Error(response.status) } )
+    return Promise.all(responses.map(response => response.text()))
+  } catch(err) {
+    throw new Error(`Failed to get months${err.code ? `: ${err.code}` : `\n${JSON.stringify(err)}`}`)
+  }
+}
+
 async function task(options = { quiet: true }) {
+  let results
   try {
     const { cookie, csrfToken } = await getCookieAndCSRFToken()
-
-    const requests = MONTHS.map(month => makeMonthRequestPromise(cookie, csrfToken, month))
-    const results = await Promise.all(requests)
-
-    const monthsWithAvailableAppts = results.map((resultHtml, idx) => {
-      const $ = cheerio.load(resultHtml)
-      return { month: MONTHS[idx], available: !!$('td[bgcolor="#ffffc0"]').text().trim() }
-    })
-
-    if (monthsWithAvailableAppts.some(monthObj => monthObj.available)) {
-      const msg = 'Appointment(s) available!'
-      notifier.notify(msg)
-      console.log(chalk.green(msg))
-      console.log(monthsWithAvailableAppts)
-      return
-    }
-
-    if (options.quiet) {
-      process.stdout.write('.')
-    } else {
-      console.log(`No available appointments for months ${MONTHS.join(', ')}`)
-    }
+    results = await fetchMonths(cookie, csrfToken)
   } catch (err) {
     if (err.statusCode) {
       console.error(chalk.red('HTTP Error:', err.statusCode))
       return
     }
 
-    console.error(chalk.red('Error\n=====\n'))
-    console.error(err)
+    console.error(chalk.red('Error\n====='))
+    console.error(typeof err.message === 'string' ? chalk.red(err.message) : err)
   }
+
+  const monthsWithAvailableAppts = results.map((resultHtml, idx) => {
+    const $ = cheerio.load(resultHtml)
+    return { month: MONTHS[idx], available: !!$('td[bgcolor="#ffffc0"]').text().trim() }
+  })
+
+  if (monthsWithAvailableAppts.some(monthObj => monthObj.available)) {
+    const msg = 'Appointment(s) available!'
+    notifier.notify(msg)
+    console.log(chalk.green(msg))
+    console.log(monthsWithAvailableAppts)
+    return
+  }
+
+  if (options.quiet) { // for running with setInterval
+    process.stdout.write('.')
+  } else { // for individual runs, or first of a setInterval run
+    console.log(`No available appointments for months ${MONTHS.join(', ')}`)
+  }
+
+  console.log('\n')
 }
 
 async function main() {
